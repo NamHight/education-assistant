f4f87bf74a379faddb572367568aed864a2bba0a
NamHight <finsr8280@gmail.com>
2025-06-04 20:52:44 +0700

    updated

--- back-end/Mappers/MapperProfile.cs
+++ back-end/Mappers/MapperProfile.cs
@@ -81,5 +81,7 @@ public class MapperProfile : Profile
         CreateMap<RequestAddNganhDto, Nganh>();
         CreateMap<RequestUpdateNganhDto, Nganh>();
         CreateMap<Nganh, ResponseNganhDto>();
+        
+        
     }
 }
